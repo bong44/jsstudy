2235055b1ab4f43692a9f81d462b3ff956121ac5
bong44 <qhdrud0429@gmail.com>
2020-07-23 14:41:32 +0900

    20.07.21 grant all 755

--- 개별프로젝트/MyScript.js
+++ 개별프로젝트/MyScript.js
@@ -1,8 +1,9 @@
-var v_container = document.getElementById("container");
-        // window.onresize=function(){
-        //     console.log("넓이",window.innerWidth);
-        //     console.log("높이",window.innerHeight);
-        // } 
+      var v_container = document.getElementById("container");
+      var v_container2 = document.getElementById("container2");
+      var v_itemList = [
+        ["T","테니스 포핸드","#"],["T","테니스 백핸드","#"],["T","테니스 서비스","#"],["T","테니스 발리","#"],["T","테니스 슬라이스","#"],["T","테니스 리턴","#"]
+        ,["T","테니스 스탭","#"]
+      ];
         function filter(){
 
         var value, name, item, i;
@@ -44,28 +45,27 @@ var v_container = document.getElementById("container");
       }
       moveImgs();
       
-      var v_itemList = [
-        ["T","테니스 포핸드","#"],["T","테니스 백핸드","#"],["T","테니스 서비스","#"],["T","테니스 발리","#"],["T","테니스 슬라이스","#"],["T","테니스 리턴","#"]
-        ,["T","테니스 스탭","#"]
-      ];
-      var v_container2 = document.getElementById("container2");
-      for (var i = 0; i < v_itemList.length; i++) {
-        var v_tempA = document.createElement("a");
-        v_tempA.setAttribute("href",v_itemList[i][2]);
-        v_tempA.setAttribute("style","text-decoration:none");
-        var v_tempDisp = document.createElement("div");
-        v_tempDisp.setAttribute("class","item");
-        var v_tempSpan1 = document.createElement("span");
-        v_tempSpan1.setAttribute("class","icon");
-        v_tempSpan1.innerHTML = v_itemList[i][0];
-        var v_tempSpan2 = document.createElement("span");
-        v_tempSpan2.setAttribute("class","name");
-        v_tempSpan2.innerHTML = v_itemList[i][1];
-        v_tempDisp.appendChild(v_tempSpan1);
-        v_tempDisp.appendChild(v_tempSpan2);
-        v_tempA.appendChild(v_tempDisp);
-        v_container2.appendChild(v_tempA);
+      function f_itemFactory() {
+        for (var i = 0; i < v_itemList.length; i++) {
+          var v_tempA = document.createElement("a");
+          v_tempA.setAttribute("href",v_itemList[i][2]);
+          v_tempA.setAttribute("style","text-decoration:none");
+          var v_tempDisp = document.createElement("div");
+          v_tempDisp.setAttribute("class","item");
+          var v_tempSpan1 = document.createElement("span");
+          v_tempSpan1.setAttribute("class","icon");
+          v_tempSpan1.innerHTML = v_itemList[i][0];
+          var v_tempSpan2 = document.createElement("span");
+          v_tempSpan2.setAttribute("class","name");
+          v_tempSpan2.innerHTML = v_itemList[i][1];
+          v_tempDisp.appendChild(v_tempSpan1);
+          v_tempDisp.appendChild(v_tempSpan2);
+          v_tempA.appendChild(v_tempDisp);
+          v_container2.appendChild(v_tempA);
+        }
       }
+
+      f_itemFactory();
 
 
 
