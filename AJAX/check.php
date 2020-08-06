3c85930e06186cc9caf4a237f204481d65a043c0
bong44 <qhdrud0429@gmail.com>
2020-08-06 12:51:50 +0900

    20.05.06

--- AJAX/check.php
+++ AJAX/check.php
@@ -1,3 +1,17 @@
 <?php
-    echo $_POST["name"];
+    // echo $_POST["name"];
+    $response = "";
+    $response = $response.$_POST["nm_name"];
+    $response = $response.$_POST["nm_al1"];
+    $response = $response.$_POST["nm_al2"];
+    $response = $response.$_POST["nm_al3"];
+    $response = $response.$_POST["nm_al4"];
+    $response = $response.$_POST["nm_al5"];
+    $response = $response.$_POST["nm_al6"];
+    $response = $response.$_POST["nm_al7"];
+    $response = $response.$_POST["nm_al8"];
+    $response = $response.$_POST["nm_al9"];
+    $response = $response.$_POST["nm_al10"];
+
+    echo $response;
 // ?>
